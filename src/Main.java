3f3096dab05335fe9f400ef224fa9f21d8336336
pk <pkji@.com>
2024-04-07 05:40:25 +0530

    Java Variable uses

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+      //variable ak container hota hai jisme store krete hai value ko
+        //A variable is a container which holds the value while the Java program is executed
+        //Types of Variables
+        //There are three types of variables in Java:
+        //local variable
+        //instance variable
+        //static variable
+
+
+        int number=10;
+        String nam="vivek";
+        boolean yesno=false;
+
+        System.out.println(nam);
+        System.out.println(number);
+        System.out.println(yesno);
+
+        }
+    }
